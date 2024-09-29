8cfc6561a20cf355dc0063edbdeb0cb2370c9954
sammy508 <chapagainsamir9@gmail.com>
2024-09-29 22:20:55 +0545

    feet to inch converter UI

---
+++ app1/codeExperiment/feet to inch converter/ft_inch.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Enter feet")
+input1 = sg.InputText(tooltip="enter")
+
+label2 = sg.Text("Enter inches")
+input2 = sg.InputText(tooltip="enter")
+
+button = sg.Button("Convert")
+
+window = sg.Window("Converter", layout=[
+    [label1,input1], [label2, input2], [button]
+])
+
+window.read()
+window.close()
+
