3f791032cf112199e08a551e257423a1942ea784
sammy508 <chapagainsamir9@gmail.com>
2024-10-16 21:27:59 +0545

    pdf-voice

--- app3(pdf-voice)/main.py
+++ app3(pdf-voice)/main.py
@@ -1,9 +1,19 @@
+from pathlib import Path
 import pandas as pnd
 import glob
+from fpdf import FPDF
 
-filepaths = glob.glob("invoices/*.xlsx")
+filepaths = glob.glob("app3(pdf-voice)/invoices/*.xlsx")
 print(len(filepaths))
 
 for filepath in filepaths:
     df = pnd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)   
+    pdf = FPDF(orientation="p",unit="mm",format="A4") 
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family = "Times",size=16, style = "B")
+    pdf.cell(w=50, h=8, txt = f"Invoice nr. {invoice_nr}")
+    pdf.output(f"app3(pdf-voice)/pdf/{filename}.pdf")
+    
+
